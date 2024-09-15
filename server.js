f753747978db3cc6ecf774991c9cb310937fff71
Samuel Bonu <samuelbonux10@gmail.com>
2024-09-15 18:56:48 +0100

    test

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const amadeus = new Amadeus({
 });
 
 
+app.use(express.json());
+
 app.get('/search-flights', async (req, res) => {
   const { origin, destination, departureDate, returnDate, adults } = req.query;
   
@@ -91,7 +93,7 @@ app.get('/flight-inspiration/:origin', async (req, res) => {
 
 //cheapest date search
 app.get('/cheapest-date', async (req, res) => {
-  const { origin, destination } = req.params;
+  const { origin, destination } = req.query;
 
   try {
     const response = await amadeus.shopping.flightDates.get({
@@ -104,6 +106,77 @@ app.get('/cheapest-date', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 })
+
+//find the cheapest flight from A -> B
+app.post('/cheapest-flight', async (req, res) => {
+  const { origin, destination, departureDate, adults } = req.body;
+
+  try {
+    const response = await amadeus.shopping.flightOffersSearch.get({
+      originLocationCode: origin,
+      destinationLocationCode: destination,
+      departureDate,
+      adults,
+    });
+    const cheapestFlight = response.data.sort((a, b) => a.price.total - b.price.total)[0];
+    res.status(200).json(cheapestFlight);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+//book a flight from A -> B
+app.post('/book-flight', async (req, res) => {
+  const { origin, destination, departureDate, adults, dateOfBirth, name, gender, contact, documents } = req.body;
+
+  try {
+    //get flight offer
+    const offerResponse = await amadeus.shopping.flightOffersSearch.get({
+      originLocationCode: origin,
+      destinationLocationCode: destination,
+      departureDate,
+      adults,
+    });
+
+    //price: get detailed pricing info
+    const priceResponse = await amadeus.shopping.flightOffers.pricing.post(
+      JSON.stringify({
+        'data': {
+          'type': 'flight-offers-pricing',
+          'flightOffers': [
+            offerResponse.data[0]
+          ]
+        }
+      })
+    );
+
+    //use pricing data to book flight
+    const bookResponse = await amadeus.booking.flightOrders.post(
+      JSON.stringify({
+        'data': {
+          'type': 'flight-order',
+          'flightOffers': [priceResponse.data.flightOffers[0]],
+          'travelers': [{
+            'id': '1',
+            'dateOfBirth': dateOfBirth,
+            'name': name,
+            'gender': gender,
+            'contact': contact,
+            'documents': documents,
+          }]
+        }
+      })
+    );
+    //responde with booking info
+    res.status(200).json(bookResponse.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`listening and serving: ${PORT}`);
 });
